ed73f6d564a224d5717095802b5ee835d9185527
TomoriNow <seanarcher1974@gmail.com>
2025-03-29 15:36:42 +1100

    Updated About and Resume March

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
                 <div className='my-auto mx-6'>
                     <h2 className='text-4xl font-bold mb-4 primary-color'>About Me</h2>
                     <p className='text-base lg:text-lg'>
-                      An avid third-year student at the Australian National University with a passion in Computer Science. I have an objective to strive for improvements in developing projects related to machine learning, refine my organizational/coordination skills, and leadership capabilities while constantly being a results-oriented individual. These qualities have led me to assume a position as a member at RISTEK Fasilkom’s Marketing and Communications division, Advanced Programming Teaching Assistant and Programming Foundations 1 & 2 Teaching Assistant at the University of Indonesia (UI), and undertake community service.
+                      An avid third-year student at the Australian National University with a passion in Computer Science. I have an objective to strive for improvements in developing projects related to machine learning, refine my organizational/coordination skills, and leadership capabilities while constantly being a results-oriented individual. These qualities have led me to assume a position as a Co-Founder/VP at the ANU Data Science Community, Advanced Programming Teaching Assistant, and Programming Foundations 1 & 2 Teaching Assistant at the University of Indonesia (UI), as well as undertake community service.
                     </p>
                 </div>
             </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -35,7 +35,7 @@ const Hero = () => {
             </p>
             
             <div className='my-8'>
-                <a href='https://drive.google.com/file/d/1qX17xp116BCoDtWjRXLjIniUQI6G3l00/view?usp=sharing' className='font-bold px-6 py-3 w-full rounded-xl mr-4 bg-gradient-to-r from-cyan-500 to-blue-500 text-white'>
+                <a href='https://drive.google.com/file/d/13LQaqljD4rjMHjT5o_AADb5DtTOEBt5T/view?usp=sharing' className='font-bold px-6 py-3 w-full rounded-xl mr-4 bg-gradient-to-r from-cyan-500 to-blue-500 text-white'>
                     Download Resume
                 </a>
                 <a href='#contact' className='font-bold px-6 py-3 w-full rounded-xl mr-4 border border-gray-400 hover:bg-gradient-to-r from-cyan-500 to-blue-500 text-white hover:border-none'>
