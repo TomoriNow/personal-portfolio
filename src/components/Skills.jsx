eef7fa5f826cf17b70ad5e50c6a74a485d7c82de
TomoriNow <seanarcher1974@gmail.com>
2025-04-07 00:42:42 +1000

    [CHORE] fixed tech stack overflow

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -20,70 +20,58 @@ const Skills = () => {
     
     <Reveal className="h-full">
     <div className='rounded-lg border border-gray-600 bg-black text-gray-400 md:h-[150px] max-w-[1200px] mx-auto grid grid-cols-6
-                    place-items-center md:flex md:justify-between md:items-center'>
+                    place-items-center md:flex md:justify-between md:items-center overflow-hidden'>
     
         <h2 className='text-white text-xl md:text-4xl font-bold m-4'>
             My <br/> Tech <br/> Stack <br/>
         </h2>
         
-        <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]'>
+        <div className='flex flex-col items-center m-4 sm:my-0 w-[50px] md:w-[100px]'>
             <img src={html} alt=""/>
-            <p className='mt-2'>HTML</p>
         </div>
         
-        <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]'>
+        <div className='flex flex-col items-center m-4 sm:my-0 w-[50px] md:w-[100px]'>
             <img src={css} alt=""/>
-            <p className='mt-2'>CSS</p>
         </div>
         
-        <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]'>
+        <div className='flex flex-col items-center m-4 sm:my-0 w-[50px] md:w-[100px]'>
             <img src={javascript} alt=""/>
-            <p className='mt-2'>JavaScript</p>
         </div>
         
-        <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]'>
+        <div className='flex flex-col items-center m-4 sm:my-0 w-[50px] md:w-[100px]'>
             <img src={tailwind} alt=""/>
-            <p className='mt-2'>Tailwind</p>
         </div>
         
-        <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]'>
+        <div className='flex flex-col items-center m-4 sm:my-0 w-[50px] md:w-[100px]'>
             <img src={react} alt=""/>
-            <p className='mt-2'>React</p>
         </div>
         
-        <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]'>
+        <div className='flex flex-col items-center m-4 sm:my-0 w-[50px] md:w-[100px]'>
             <img src={python} alt=""/>
-            <p className='mt-2'>Python</p>
         </div>
         
-        <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]'>
+        <div className='flex flex-col items-center m-4 sm:my-0 w-[50px] md:w-[100px]'>
             <img src={java} alt=""/>
-            <p className='mt-2'>Java</p>
         </div>
         
-        <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]'>
+        <div className='flex flex-col items-center m-4 sm:my-0 w-[50px] md:w-[100px]'>
             <img src={flutter} alt=""/>
-            <p className='mt-2'>Flutter</p>
         </div>
         
-        <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]'>
+        <div className='flex flex-col items-center m-4 sm:my-0 w-[50px] md:w-[100px]'>
             <img src={go} alt=""/>
-            <p className='mt-2'>Go</p>
         </div>
         
-        <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]'>
+        <div className='flex flex-col items-center m-4 sm:my-0 w-[50px] md:w-[100px]'>
             <img src={django} alt=""/>
-            <p className='mt-2'>Django</p>
         </div>
         
-        <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]'>
+        <div className='flex flex-col items-center m-4 sm:my-0 w-[50px] md:w-[100px]'>
             <img src={sql} alt=""/>
-            <p className='mt-2'>PostgreSQL</p>
         </div>
         
-        <div className='flex flex-col items-center m-4 sm:my-0 w-[40px] md:w-[100px]'>
+        <div className='flex flex-col items-center m-4 sm:my-0 w-[50px] md:w-[100px]'>
             <img src={spring} alt=""/>
-            <p className='mt-2'>Springboot</p>
         </div>
     </div>
     </Reveal>
