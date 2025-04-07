21b884cf639a1153a8ededcade3e1632836dadbf
TomoriNow <seanarcher1974@gmail.com>
2025-04-07 20:40:05 +1000

    [FEAT] Added Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import Skills from "./components/Skills"
-import About from "./components/About"
-import Projects from "./components/Projects"
-import Contact from "./components/Contact"
-import Footer from "./components/Footer"
+import { Analytics } from "@vercel/analytics/react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Skills from "./components/Skills";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
 
@@ -18,8 +19,8 @@ function App() {
         <Projects/>
         <Contact/>
         <Footer/>
+        <Analytics/>
       </div>
-      
     </>
   )
 }
