2febd86f6f3850a2d2cab6893846fda4e06dfd1c
TomoriNow <seanarcher1974@gmail.com>
2025-07-25 07:53:03 +1000

    [CHORE] Updated Resume Text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -41,7 +41,7 @@ const Hero = () => {
             
             <Reveal className="h-full">
             <div className='my-8'>
-                <a href='https://drive.google.com/file/d/1D2DOt8uQwn6RbVoY9gaFb3JTnI1BRGrU/view?usp=sharing' className='font-bold px-6 py-3 w-full rounded-xl mr-4 bg-gradient-to-r from-cyan-500 to-blue-500 text-white'>
+                <a href='https://drive.google.com/file/d/1oFo8BYDMEh0B26Wm0sfSs9UnpC1-pNc5/view?usp=sharing' className='font-bold px-6 py-3 w-full rounded-xl mr-4 bg-gradient-to-r from-cyan-500 to-blue-500 text-white'>
                     Download Resume
                 </a>
                 <a href='#contact' className='font-bold px-6 py-3 w-full rounded-xl mr-4 border border-gray-400 hover:bg-gradient-to-r from-cyan-500 to-blue-500 text-white hover:border-none'>
