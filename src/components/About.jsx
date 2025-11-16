c471dac24d823628682ebf974a6c9e3306c9a33c
TomoriNow <seanarcher1974@gmail.com>
2025-11-16 19:54:08 +0700

    [CHORE] Resume Update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,14 +11,7 @@ const About = () => {
                 <div className='my-auto mx-6'>
                     <h2 className='text-4xl font-bold mb-4 primary-color'>About Me</h2>
                     <p className='text-base lg:text-lg'>
-                      I am a fourth-year student at the Australian National University with a passion in Computer
-                      Science. I have an objective to strive for improvements in developing projects related to
-                      machine learning, refine my organizational/coordination skills, and leadership capabilities
-                      while constantly being a results-oriented individual. These qualities have led me to assume a
-                      position as a Co-Founder/VP at the ANU Data Science Community, an Academic Tutor for
-                      Introduction to Machine Learning at the Australian National University, an Advanced
-                      Programming Teaching Assistant at the University of Indonesia (UI), as well as undertake
-                      community service.
+                      I am a fourth-year student at the Australian National University (ANU) with a passion in Computer Science. I have an objective to strive for improvements in developing projects related to machine learning, and refine my organizational and leadership capabilities while constantly being a results-oriented individual. These qualities have led me to assume a position as a Software Engineer Intern (AI) at SIRCLO, a Co-Founder/VP of the ANU Data Science Community, an Academic Tutor for Introduction to Machine Learning at the ANU, and an Advanced Programming Teaching Assistant at the University of Indonesia (UI).
                     </p>
                 </div>
             </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -41,7 +41,7 @@ const Hero = () => {
             
             <Reveal className="h-full">
             <div className='my-8'>
-                <a href='https://drive.google.com/file/d/1oFo8BYDMEh0B26Wm0sfSs9UnpC1-pNc5/view?usp=sharing' className='font-bold px-6 py-3 w-full rounded-xl mr-4 bg-gradient-to-r from-cyan-500 to-blue-500 text-white'>
+                <a href='https://drive.google.com/file/d/1aFW220bFy0J5OJyrqDifrkJK2-n7tqvx/view?usp=sharing' className='font-bold px-6 py-3 w-full rounded-xl mr-4 bg-gradient-to-r from-cyan-500 to-blue-500 text-white'>
                     Download Resume
                 </a>
                 <a href='#contact' className='font-bold px-6 py-3 w-full rounded-xl mr-4 border border-gray-400 hover:bg-gradient-to-r from-cyan-500 to-blue-500 text-white hover:border-none'>
